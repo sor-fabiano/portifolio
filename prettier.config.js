06c4af9fe24809c56a3a9d1c70fd6ae7bba29c4a
Fabiano Parreira <fabiano.parreira@soreto.com>
2021-10-10 15:56:37 -0300

    setting prettier eslint

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "singleQuote": true,
+  "trailingComma": 'all',
+	"arrowParens": 'avoid',
+  "bracketSameLine": false,
+  "bracketSpacing": true,
+  "embeddedLanguageFormatting": "auto",
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+  "jsxSingleQuote": false,
+  "printWidth": 80,
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+};
+export default MyApp;
